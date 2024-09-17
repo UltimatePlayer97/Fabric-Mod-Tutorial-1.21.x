cec0fe29e218000077700b0da03a2cf018474397
UltimatePlayer97 <gogole.help134@gmail.com>
2024-09-17 16:16:28 +1000

    Add custom items and licensing changes

--- src/main/java/net/systemerror/tutorialmod/TutorialMod.java
+++ src/main/java/net/systemerror/tutorialmod/TutorialMod.java
@@ -2,14 +2,17 @@ package net.systemerror.tutorialmod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.systemerror.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class TutorialMod implements ModInitializer {
+
 	public static final String MOD_ID = "tutorialmod";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
+		ModItems.registerModItems();
 	}
 }
